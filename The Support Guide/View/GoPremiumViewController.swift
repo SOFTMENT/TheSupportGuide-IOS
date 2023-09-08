c8067598ced76d03264977348ece4de7cd232d75
Vijay Rathore <iamvijay67@gmail.com>
2023-09-09 00:06:35 +0530

    Commit for 2023-09-09 00:06:17

--- The Support Guide/View/GoPremiumViewController.swift
+++ The Support Guide/View/GoPremiumViewController.swift
@@ -45,7 +45,7 @@ class GoPremiumViewController: UIViewController {
            dateFormatter.dateFormat = "yyyy-MM-dd"
            dateFormatter.timeZone = TimeZone.current
            dateFormatter.locale = Locale.current
-        if let finalDate =  dateFormatter.date(from: "2023-09-07") {
+        if let finalDate =  dateFormatter.date(from: "2023-09-09") {
             if finalDate > Date() {
                 tipMainView.isHidden = true
             }
